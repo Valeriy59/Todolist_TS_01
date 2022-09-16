eb64baa4c4de504f9a3a6406d05f9d083cad07b1
Valeriy Litvinov <valitvinoff@mail.ru>
2022-09-16 09:42:52 +0300

    Initial commit

---
+++ src/components/Accordion.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Accordion() {
+    return (
+        <div>
+            <h3>Меню</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
+}
+export default Accordion
